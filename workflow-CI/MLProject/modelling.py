c3458caae3800ee3d9af4b0df0813c930428f08b
MIFDAL <saidilmifdal15@gmail.com>
2025-12-24 19:35:55 +0800

    Update Log Modelling

--- workflow-CI/MLProject/modelling.py
+++ workflow-CI/MLProject/modelling.py
@@ -18,6 +18,8 @@ def main(train_data, test_data):
     X_test = test_df.drop(columns=[target_col])
     y_test = test_df[target_col]
     # mlflow.sklearn.autolog()  # Disabled to avoid autolog overriding manual log_model
+    # Force MLflow tracking URI to local mlruns folder (relative to project root)
+    mlflow.set_tracking_uri("file:./mlruns")
 
     with mlflow.start_run(run_name="LogisticRegression_CI") as run:
         model = LogisticRegression(max_iter=1000, random_state=42)
